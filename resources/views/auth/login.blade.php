c6cabf25c7a472581dd3130e4c603cfb9a46e699
Rodrigo <r.ms01@hotmail.com>
2024-10-01 21:48:09 -0500

    Sprint 1:Tipos 1.3-Información Agregada

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,7 +7,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+                <div class="card-header">{{ __('Inicio de Sesión') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
@@ -47,7 +47,7 @@
                                     <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
                                     <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
+                                        {{ __('Recordar Sesión') }}
                                     </label>
                                 </div>
                             </div>
@@ -56,7 +56,7 @@
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
+                                    {{ __('Iniciar Sesión') }}
                                 </button>
 
                                 @if (Route::has('password.request'))
